797fd7bfc6bb8b6242ed11dc2da4e3724a31cfcf
恽昕宇 <1004804175@qq.com>
2022-07-22 17:17:26 +0800

    表格中“状态"显示样式更新为4个,修改myfilter中处理逻辑以适配不同表格中的样式显示

--- supply/templatetags/myfilter.py
+++ supply/templatetags/myfilter.py
@@ -7,12 +7,7 @@ def front_add(arg,val):
 
 @register.filter(name="table_status")
 def table_status(arg,val):
-    choices = (
-        (0, "未审核"),
-        (1, "已审核"),
-        (2, "已完成")
-    )
     #CSS中样式： 0-黄色  1-蓝色  2-绿色  3-红色
-    cgxq={0:0,1:1,2:2} #采购需求对应(0, "未审核"),(1, "已审核"),(2, "已完成")
-
-    return "table_status_{}".format(arg)
+    d=dict()
+    d['cgxq']={0:0,1:1,2:2} #采购需求对应(0, "未审核"),(1, "已审核"),(2, "已完成")
+    return "table_status_{}".format(d[val][arg])
