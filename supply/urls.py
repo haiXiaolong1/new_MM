afd726a26e931432c367f393f230c4a4a0cd9000
恽昕宇 <1004804175@qq.com>
2022-07-26 03:54:14 +0800

    【员工信息交互】发送信息功能

--- supply/urls.py
+++ supply/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('setMessageDetail', views.set_message_detail),
     path('setMessageList', views.url_set_message_list),
     path('sendTestMessage', views.send_test_message),
+    path('sendMessage', views.send_message),
 ]
