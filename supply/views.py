ce70cfc2e18423b059630bf1c95d01130ae86c30
恽昕宇 <1004804175@qq.com>
2022-07-30 18:50:40 +0800

    微调自动发信文字内容

--- supply/views.py
+++ supply/views.py
@@ -361,6 +361,7 @@ def material_add(request):
     # 判断是否已经存在此供应关系，如果已存在则提示不能重复添加
     isexist = models.Gongyingguanxi.objects.filter(supplyid_id=sid, materialid_id=mid)
     if isexist:
+        request.session['notify'] = [dict(id=0, tittle="提示", context="供应关系已存在，不能重复创建", type="error", position="top-center")]
         return JsonResponse({"status": True, "isexist": True})
     toCheck = [sid, mid]
     types = ['id供应商编号', 'id物料编号']
@@ -371,6 +372,7 @@ def material_add(request):
         models.Gongyingguanxi.objects.create(createtime=time, updatetime=time,
                                              createid_id=id, updateid_id=id,
                                              supplyid_id=sid, materialid_id=mid)
+        request.session['notify'] = [dict(id=0, tittle="提示", context="供应关系创建成功", type="success", position="top-center")]
     return JsonResponse(res)
 
 
@@ -447,7 +449,7 @@ def quote_add(request):
         notify.append(dict(id=1, tittle="系统消息", context="向 {}-{} 发信反馈已报价".format(yg.get_office_display(),yg.username), type="info", position="top-center"))
         notify.append(dict(id=2, tittle="系统消息", context="已提示 {}-{} 前往评估报价单".format(jl.get_office_display(),jl.username), type="info", position="top-center"))
         request.session["notify"] =notify
-        message.append("【系统消息】<br/>收到新报价单")
+        message.append("【系统消息】收到新报价单")
         message.append('供应商:{}<br/>({})已报价<br/>询价单号:{}<br/>报价单号:{}<br/>预期报价:{}元/{}<br/>供应商报价:{}元/{}<br/>请评估报价<a class="chat_link" href="/purchase/quote/evaluate/">>></a>'
                        .format(gys.name,gys.id,xjd.inquiryid,qid,qgd.price,wl.calcutype,quote,wl.calcutype))
         for m in message:
@@ -502,7 +504,7 @@ def mm_add(request):
     id = request.session["info"]['id']
     models.Wuliao.objects.create(type=o['type'], salegroup=o['salegroup'], saleway=o['saleway']
                                  , id=sid, calcutype=o['calcutype'], desc=o['desc'])
-    print(request.POST)
+
     return JsonResponse({"status": True})
 
 
