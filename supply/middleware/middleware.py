68f1e77377fd4e58f829d462d1d5c75b526fe029
haixiaolong <2393226759@qq.com>
2022-08-10 21:57:47 +0800

    更改中间件，不登陆也能登后台管理

--- supply/middleware/middleware.py
+++ supply/middleware/middleware.py
@@ -1,7 +1,7 @@
 
 from django.shortcuts import redirect
 from django.utils.deprecation import  MiddlewareMixin
-
+import re
 class AuthMW(MiddlewareMixin):
     # 通过自定义中间件进行登录认证
 
@@ -9,7 +9,8 @@ class AuthMW(MiddlewareMixin):
         if request.path_info == "/login/" or request.path_info == "/account/ac/password/" or \
                 request.path_info == "/account/ac/send/" or request.path_info == "/account/ac/login/":
             return
-
+        if re.match("^(/admin)(.)*",request.path_info):
+            return
         if request.path_info == "/forgot":
             return
         if request.path_info == "/supply/r_password":
